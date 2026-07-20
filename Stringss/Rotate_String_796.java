c648e0eec6b44c9f1a484313e29be21220c0d4ee
sudhir-tech <sudhirsahoo523@gmail.com>
2026-07-20 21:28:40 +0530

    Solved LeetCode 796 - Rotate String (Java)

---
+++ Stringss/Rotate_String_796.java
@@ -0,0 +1,12 @@
+package Stringss;
+
+public class Rotate_String_796 {
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        String concatenated = s + s;
+        return concatenated.contains(goal);
+    }
+    
+}
