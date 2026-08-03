970c3e13ef8f6aa3897f87de975499fb8222fbef
sudhir-tech <sudhirsahoo523@gmail.com>
2026-08-03 10:10:36 +0530

    Solved 0006 - 'Solved multiple problems'

---
+++ Arrays/ContainerWithMostWater011.java
@@ -0,0 +1,23 @@
+public class ContainerWithMostWater011 {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int width = right - left;
+            int currentHeight = Math.min(height[left], height[right]);
+            int currentArea = width * currentHeight;
+            maxArea = Math.max(maxArea, currentArea);
+
+            // Move the pointer pointing to the shorter line
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
