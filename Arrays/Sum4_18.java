567f937b9d1fa7dbb96268f99ff351608d7ae73a
sudhir-tech <sudhirsahoo523@gmail.com>
2026-08-04 14:16:51 +0530

    Solved 0006 - 'Solved multiple problems'

--- Arrays/Sum4_18.java
+++ Arrays/Sum4_18.java
@@ -1,33 +1,48 @@
+import java.util.*;
 
 class Solution {
-    public List<List<Integer>> fourSum(int[] nums, int target) { 
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+
         List<List<Integer>> result = new ArrayList<>();
+
         Arrays.sort(nums);
+
         int n = nums.length;
-        
+
         for (int i = 0; i < n - 3; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue; // Skip duplicates for the first number
-            }
+
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
             for (int j = i + 1; j < n - 2; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) {
-                    continue; // Skip duplicates for the second number
-                }
+
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
+
                 int left = j + 1;
                 int right = n - 1;
-                
+
                 while (left < right) {
-                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+
                     if (sum == target) {
-                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+
+                        result.add(Arrays.asList(
+                                nums[i],
+                                nums[j],
+                                nums[left],
+                                nums[right]));
+
                         left++;
                         right--;
-                        while (left < right && nums[left] == nums[left - 1]) {
-                            left++; // Skip duplicates for the third number
-                        }
-                        while (left < right && nums[right] == nums[right + 1]) {
-                            right--; // Skip duplicates for the fourth number
-                        }
+
+                        while (left < right && nums[left] == nums[left - 1])
+                            left++;
+
+                        while (left < right && nums[right] == nums[right + 1])
+                            right--;
+
                     } else if (sum < target) {
                         left++;
                     } else {
@@ -36,8 +51,7 @@ class Solution {
                 }
             }
         }
-        
+
         return result;
-        
     }
 }
