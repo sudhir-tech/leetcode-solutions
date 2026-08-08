181119971f81b6b588d530fcd587eef5f3f83113
sudhir-tech <sudhirsahoo523@gmail.com>
2026-08-08 21:49:51 +0530

    Solved Multiple problems

---
+++ Arrays/SearchInRotatedSortedArray33.java
@@ -0,0 +1,33 @@
+public class SearchInRotatedSortedArray33 {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // Check if the left half is sorted
+            if (nums[left] <= nums[mid]) {
+                // Target is in the left half
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else { // Right half is sorted
+                // Target is in the right half
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1; // Target not found
+    }
+}
