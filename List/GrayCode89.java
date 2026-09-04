6bb69a465c76b2375971e5f7cf608dd412fa4aed
sudhir-tech <sudhirsahoo523@gmail.com>
2026-09-04 19:26:38 +0530

    leetcode solutions

---
+++ List/GrayCode89.java
@@ -0,0 +1,12 @@
+package List;
+
+public class GrayCode89 {
+    public List<Integer> grayCode(int n) {
+        List<Integer> result = new ArrayList<>();
+        int total = 1 << n; // 2^n
+        for (int i = 0; i < total; i++) {
+            result.add(i ^ (i >> 1)); // Gray code formula
+        }
+        return result;
+    }
+}
