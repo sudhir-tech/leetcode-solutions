913e8ba1bef0893081b84cc30061c451eb302d13
sudhir-tech <sudhirsahoo523@gmail.com>
2026-09-04 19:16:49 +0530

    leetcode solutions

---
+++ Arrays/Permutations46.java
@@ -0,0 +1,23 @@
+public class Permutations46 {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(nums, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(int[] nums, List<Integer> current, List<List<Integer>> result) {
+        if (current.size() == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (current.contains(nums[i])) {
+                continue; // skip duplicates
+            }
+            current.add(nums[i]);
+            backtrack(nums, current, result);
+            current.remove(current.size() - 1);
+        }
+    }
+    
+}
