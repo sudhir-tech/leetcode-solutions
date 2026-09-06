6f528999c6a69fcfe305316c7320827a4d802a68
sudhir-tech <sudhirsahoo523@gmail.com>
2026-09-06 19:21:48 +0530

    leetcode solutions

---
+++ Strings/EditDistance72.java
@@ -0,0 +1,29 @@
+package Strings;
+
+public class EditDistance72 {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i; // Deleting all characters from word1
+        }
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j; // Inserting all characters to word1 to form word2
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1]; // Characters match, no operation needed
+                } else {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    // Minimum of delete, insert, or replace
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
