03051ffa4f0d664c76556299f68ffacc27f15960
sudhir-tech <sudhirsahoo523@gmail.com>
2026-09-06 19:20:00 +0530

    leetcode solutions

---
+++ Arrays/SortColors75.java
@@ -0,0 +1,25 @@
+public class SortColors75 {
+    public void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+
+        while (mid <= high) {
+            switch (nums[mid]) {
+                case 0:
+                    swap(nums, low++, mid++);
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2:
+                    swap(nums, mid, high--);
+                    break;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
