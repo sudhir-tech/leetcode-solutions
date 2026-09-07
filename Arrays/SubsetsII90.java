a2331c19a8df9a194349099b91987d299717c3fd
sudhir-tech <sudhirsahoo523@gmail.com>
2026-09-07 21:00:09 +0530

    leetcode solutions

---
+++ Arrays/SubsetsII90.java
@@ -0,0 +1,18 @@
+public class SubsetsII90 {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums); // Sort to handle duplicates
+        backtrack(result, new ArrayList<>(), nums, 0);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] nums, int start) {
+        result.add(new ArrayList<>(tempList));
+        for (int i = start; i < nums.length; i++) {
+            if (i > start && nums[i] == nums[i - 1]) continue; // Skip duplicates
+            tempList.add(nums[i]);
+            backtrack(result, tempList, nums, i + 1);
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+}
