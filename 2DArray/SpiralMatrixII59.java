85d700a5a0fc855bfbd0137c3583e6a16ac30beb
sudhir-tech <sudhirsahoo523@gmail.com>
2026-09-09 18:39:30 +0530

    leetcode solutions

---
+++ 2DArray/SpiralMatrixII59.java
@@ -0,0 +1,39 @@
+public class SpiralMatrixII59 {
+    public int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        int left = 0, right = n - 1, top = 0, bottom = n - 1;
+        int num = 1;
+
+        while (left <= right && top <= bottom) {
+            // Fill top row
+            for (int i = left; i <= right; i++) {
+                matrix[top][i] = num++;
+            }
+            top++;
+
+            // Fill right column
+            for (int i = top; i <= bottom; i++) {
+                matrix[i][right] = num++;
+            }
+            right--;
+
+            // Fill bottom row
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    matrix[bottom][i] = num++;
+                }
+                bottom--;
+            }
+
+            // Fill left column
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    matrix[i][left] = num++;
+                }
+                left++;
+            }
+        }
+
+        return matrix;
+    }
+}
